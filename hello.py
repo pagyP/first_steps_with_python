4062d587816d1ebcead31570704a79981894d5cd
Paul Paginton <paul.x.paginton@gsk.com>
2020-05-27 15:50:55 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+#print("hello there!")
+print("What is your name?")
+name = input()
+print("Hello, " + name)
+
+#Keywords are commands built into the programming language and they have a special meaning.  They can't
+#be used as variable names
+
+#Operators
+#An operator is a character that instructs the Python complier to perform some special operation
+#Opening and closing parenthese () are function invocation operators when places to the right of the function name
+#= is the assignment operator.  It is used to assign a value to a variable
+# The = sign is the string concatenation operator.  Also the addition operator
+# Is the comment operator
