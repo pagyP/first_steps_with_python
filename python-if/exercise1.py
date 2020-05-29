dced37d213a9b86f5fc3c85b69fb05012b36cd5a
Paul Paginton <paul.x.paginton@gsk.com>
2020-05-29 13:40:41 +0100

    if, else and elif

---
+++ python-if/exercise1.py
@@ -0,0 +1,13 @@
+value = "12"
+
+if value == "7":
+    print("the value is 7") #In python the code to run if the Boolean is true is identified by the indentation
+    #if the boolean is False all code in the indentation is ignored
+elif value =="8":
+    print("the value is 8")
+elif value =="9":
+    print("the value is 9")
+else:
+    print("the value is not one we're looking for") # else is used as an option if the boolean is false
+
+print("Finished!")
