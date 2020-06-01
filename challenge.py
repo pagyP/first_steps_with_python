479ea7fde8976c751154750c919af72221e7c045
Paul Paginton <paul.x.paginton@gsk.com>
2020-06-01 09:16:24 +0100

    challenge.py

---
+++ challenge.py
@@ -0,0 +1,12 @@
+print("Would you like to continue?")
+carryon = input()
+if carryon == "no" or carryon == "n":
+    print("Exiting")
+elif carryon == "yes" or carryon == "y":
+    print("Continuing.. + \n Complete")
+else:
+    print("Please try again and respond with yes or no")
+
+
+
+
