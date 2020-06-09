11f9b27c86dc6e88045534c9e0c191e567e318ad
Paul Paginton <paul.x.paginton@gsk.com>
2020-06-09 20:02:28 +0100

    python lists

---
+++ python-lists/exercise1.py
@@ -0,0 +1,64 @@
+# colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'brown']
+
+# print(colors)
+# print(type(colors))
+
+#Lots of different data types can be in a list but it's not preferable or practical
+# sundry = ['John', 3.14, 7, False]
+# print(sundry)
+# print(type(sundry))
+
+#Initialise an empty list
+#my_list = []
+
+colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'brown']
+
+#Use indexes to access individual elements in the list
+# print(f'0-based indexing into the list ... second item: {colors[1]}')
+
+# print(f'Last item of the list: {colors[-1]}')
+
+# print(f'Next to last item in the list: {colors[-2]}')
+
+# print('\nPrint a SLICE, starting at index 2 and excluding index 5:')
+# print(colors[2:5])
+# print(type(colors[2:5]))
+
+# print('\nPrint a slice, starting at index 0 to index 3:')
+# print(colors[:3])
+
+# print('\nPrint a slice, starting a index 4 to the end of the list:')
+# print(colors[4:])
+
+# print('\nPrint a slice, from the 4th from the end up until the last item:')
+# print(colors[-4:-1])
+
+# colors.reverse()
+# print(colors)
+
+# colors.sort()
+# print(colors)
+
+# print(colors)
+
+# color = colors.pop(0)
+# print('popped', color)
+
+# print(colors)
+
+# print(colors)
+
+# colors.append('black')
+# colors.append('white')
+
+# colors.remove('yellow')
+# colors.remove('orange')
+
+# print(colors)
+
+# new_colors = ['lime', 'gray']
+# colors.extend(new_colors)
+# print(colors)
+
+colors.clear()
+print(colors)
